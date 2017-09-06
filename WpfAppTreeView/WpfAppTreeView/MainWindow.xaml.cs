314e20ca85900c6bf0443874dae7f86c3697cadb
choro <choro@DESKTOP-O2TD37A>
2017-09-06 21:54:09 +0300

    Элемент управления  «TreeView» с привязкой к данным.

--- WpfAppTreeView/WpfAppTreeView/MainWindow.xaml.cs
+++ WpfAppTreeView/WpfAppTreeView/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfAppTreeView
 {
@@ -23,6 +10,30 @@ namespace WpfAppTreeView
         public MainWindow()
         {
             InitializeComponent();
+            TreeViewModel dc = new TreeViewModel();
+            // Создадим узлы для иерархического списка
+            // Уровень 1
+            TreeViewItemModel level1_1 = new TreeViewItemModel() { Number = 1, Name = "Уровень 1" };
+            TreeViewItemModel level1_2 = new TreeViewItemModel() { Number = 2, Name = "Уровень 1" };
+            // Уровень 2
+            TreeViewItemModel level2_1 = new TreeViewItemModel() { Number = 1, Name = "Уровень 2" };
+            TreeViewItemModel level2_2 = new TreeViewItemModel() { Number = 2, Name = "Уровень 2" };
+            TreeViewItemModel level2_3 = new TreeViewItemModel() { Number = 3, Name = "Уровень 2" };
+            TreeViewItemModel level2_4 = new TreeViewItemModel() { Number = 4, Name = "Уровень 2" };
+            // Уровень 3
+            TreeViewItemModel level3_1 = new TreeViewItemModel() { Number = 1, Name = "Уровень 3" };
+            TreeViewItemModel level3_2 = new TreeViewItemModel() { Number = 2, Name = "Уровень 3" };
+            // Собираем иерархический список
+            level2_3.ListTreeViewItem.Add(level3_1);
+            level2_3.ListTreeViewItem.Add(level3_2);
+            level1_1.ListTreeViewItem.Add(level2_1);
+            level1_1.ListTreeViewItem.Add(level2_2);
+            level1_1.ListTreeViewItem.Add(level2_3);
+            level1_1.ListTreeViewItem.Add(level2_4);
+            dc.Root_ListTreeViewItem.Add(level1_1);
+            dc.Root_ListTreeViewItem.Add(level1_2);
+            // Контекст данных
+            DataContext = dc;
         }
     }
 }
---
+++ WpfAppTreeView/WpfAppTreeView/TreeViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace WpfAppTreeView
+{
+    /// <summary>
+    /// Модель данных для иерархического списка
+    /// </summary>
+    class TreeViewModel
+    {
+        /// <summary>
+        /// Коллекция элементов иерархического списка
+        /// </summary>
+        public List<TreeViewItemModel> Root_ListTreeViewItem { get; set; }
+
+        public TreeViewModel()
+        {
+            // Инициализируем коллекцию элементов
+            Root_ListTreeViewItem = new List<TreeViewItemModel>();
+        }
+    }
+    /// <summary>
+    /// Элемент иерархического списка
+    /// </summary>
+    class TreeViewItemModel
+    {
+        /// <summary>
+        /// Поле для примера
+        /// </summary>
+        public int Number { get; set; }
+        /// <summary>
+        /// Поле для примера
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Поле для примера
+        /// </summary>
+        public string FullName
+        {
+            get { return string.Format("{0}_{1}", Name, Number); }
+        }
+        /// <summary>
+        /// Коллекция элементов иерархического списка для текущего элемента
+        /// </summary>        
+        public List<TreeViewItemModel> ListTreeViewItem { get; set; }
+
+        public TreeViewItemModel()
+        {
+            // Инициализируем коллекцию элементов
+            ListTreeViewItem = new List<TreeViewItemModel>();
+        }
+    }
+}
